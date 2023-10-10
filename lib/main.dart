1a432bde28b9b00e9e39c6f3e0a8216c46247d20
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2023-10-10 14:12:46 +0800

    pubspec.yaml: assets: images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:doctor_app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Doctor App',
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(255, 255, 255, 1)),
+      ),
+      home: HomePage(),
+    );
+  }
 }
